325268e03865396c94e26754a0cd1aac4eab8423
mao <37127916+maoz99@users.noreply.github.com>
2019-05-07 14:34:54 -0500

    updated_sourse.cpp

--- Source.cpp
+++ Source.cpp
@@ -20,9 +20,12 @@ int main()
 	vector<Course> courseInfo;// to store course
 	vector<Assignment> assigning;// to store course - instructor assignment during iteration 
 	vector <Assignment> optimal; // to store optimal assignment
-
+	
+	
+	
 	instructorInput(instructorInfo);
 	int numberOfCourses = instructorInfo.size(); // number of courses = number of instructors 
+	
 	
 	courseInput(courseInfo, numberOfCourses);
 	cout << endl;
@@ -31,6 +34,22 @@ int main()
 	cout << endl;
 	cout << "\t................... Percentage of course content............." << endl;
 	displayCourseInfo(courseInfo);
+
+	//couseid and course name
+	cout << endl;
+	cout << "\t............Course ID...........Course name......." << endl;
+	cout << endl;
+	for (int n = 0; n < numberOfCourses; n++) {
+		cout << "\t\t\t"<< courseInfo[n].getId() << "\t\tProblem solving and Programming  " << n + 1 << endl;
+	}
+	//instructor id and name
+	cout << endl;
+	cout << "\t...........Instructor ID.........Instructor name......." << endl;
+	cout << endl;
+	for (int n = 0; n < numberOfCourses; n++) {
+		cout << "\t\t\t" << instructorInfo[n].getId() << "\t\tInstructor  " << n + 1 << endl;
+	}
+
 
 	int assignmetSum = 0; // to store match score for each iteration
 	int optimalSum = 0;// to store optimal match score
@@ -59,20 +78,14 @@ int main()
 	
 	}
 	cout << endl;
-	cout<<"\t\t.............................."<<endl;
-	cout << "CourseID\t InstructorID \t MatchScore" << endl;
+	cout<<"......................................."<<endl;
+	cout << "CourseID\t InstructorID \t MatchScore" << endl<<endl;
 	
 	for (int i = 0; i < numberOfCourses; i++)
 	{
 		
 		cout << optimal[i].getCourseId() << " \t\t" << optimal[i].getInstructorId() << "\t\t" << optimal[i].getMatchScore() << endl;
 	}
-
-
-
-	
-
-
 	system("pause");
 	return 0;
 }
